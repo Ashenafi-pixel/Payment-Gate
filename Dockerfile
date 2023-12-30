9842e621e12088ccb592d838a0e23b83c5693eed
Tesfamichael Aboset <tesfa722@gmail.com>
2023-12-30 06:38:48 +0000

    Updated dockerfile

--- Dockerfile
+++ Dockerfile
@@ -27,6 +27,7 @@ RUN npm run production
 
 # Change ownership of storage and cache directories
 RUN chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+
 # Set the correct permissions for the public directory
 RUN chmod -R 755 /var/www/html/public
 
--- resources/views/frontend/layouts/foot.blade.php
+++ resources/views/frontend/layouts/foot.blade.php
@@ -1,9 +1,20 @@
-{!! Html::script(asset('frontend/js/jquery-3.6.3.min.js')) !!}
-{!! Html::script(asset('frontend/js/bootstrap.bundle.min.js')) !!}
-{!! Html::script(asset('frontend/js/all.min.js')) !!}
-{!! Html::script(asset('frontend/js/aos.js')) !!}
-{!! Html::script(asset('frontend/js/slick.min.js')) !!}
-{!! Html::script(asset('frontend/js/custom.js')) !!}
-{!! Html::script(asset('app-assets/js/noty-js/main.js')) !!}
-{!! Html::script(asset('app-assets/js/custom/global.js')) !!}
+<!-- {!! Html::script(asset('frontend/js/jquery-3.6.3.min.js')) !!} -->
+<!-- {!! Html::script(asset('frontend/js/bootstrap.bundle.min.js')) !!} -->
+<!-- {!! Html::script(asset('frontend/js/all.min.js')) !!} -->
+<!-- {!! Html::script(asset('frontend/js/aos.js')) !!} -->
+<!-- {!! Html::script(asset('frontend/js/slick.min.js')) !!} -->
+<!-- {!! Html::script(asset('frontend/js/custom.js')) !!} -->
+<!-- {!! Html::script(asset('app-assets/js/noty-js/main.js')) !!} -->
+<!-- {!! Html::script(asset('app-assets/js/custom/global.js')) !!} -->
+<!-- @stack('js') -->
+
+<!-- Update the asset URLs to use http -->
+{!! Html::script('http://192.168.100.35:4050/frontend/js/jquery-3.6.3.min.js') !!}
+{!! Html::script('http://192.168.100.35:4050/frontend/js/bootstrap.bundle.min.js') !!}
+{!! Html::script('http://192.168.100.35:4050/frontend/js/all.min.js') !!}
+{!! Html::script('http://192.168.100.35:4050/frontend/js/aos.js') !!}
+{!! Html::script('http://192.168.100.35:4050/frontend/js/slick.min.js') !!}
+{!! Html::script('http://192.168.100.35:4050/frontend/js/custom.js') !!}
+{!! Html::script('http://192.168.100.35:4050/app-assets/js/noty-js/main.js') !!}
+{!! Html::script('http://192.168.100.35:4050/app-assets/js/custom/global.js') !!}
 @stack('js')
