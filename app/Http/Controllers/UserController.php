f560e51c7b2751259d366e4399ec7889d3d2472d
Ashenafi Alemu <ashenafialemu27@gmail.com>
2024-02-03 07:55:06 +0000

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use App\Models\User;
 use App\Models\MerchantDetail;
-
+use Carbon\Carbon;
 class UserController extends Controller
 {
     public function registerUser(Request $request)
@@ -20,25 +20,23 @@ class UserController extends Controller
         Log::info('Extracted user data: ' . json_encode($userData));
         Log::info('Extracted merchant data: ' . json_encode($merchantData));
         Log::info('Extracted username: ' . $userData['username']);
-
-
-        $disk = 'public/images';
-
-        // Save the image to the specified disk
-
             $passportFile = $merchantData['passport'];
-            $decodedImagePassport = base64_decode($passportFile);
-            $passportFileName = date('YmdHis').'.jpg';
+            $decodedPassportImage = base64_decode($passportFile);
+            $currentDateTime = Carbon::now()->format('YmdHisu');
+            $randomNumber = mt_rand(0, 100);
+            $result = $currentDateTime . '_' . $randomNumber;
+            $passportFileName = date('YmdHis') .$result .'.jpg';
             //$decodedImage->move(public_path('image'), $passportFileName);
             $passport = 'images/' . $passportFileName;
-            
             $licenseFile = $merchantData['license'];
-            $decodedImageLicense = base64_decode($licenseFile);
-            $licenseFileName = date('YmdHis') .'.jpg';
+            $decodedLicenseImage = base64_decode($licenseFile);
+            $currentDateTime = Carbon::now()->format('YmdHisu');
+            $randomNumber = mt_rand(0, 100);
+            $result = $currentDateTime . '_' . $randomNumber;
+            $licenseFileName = date('YmdHis')  .$result.'.jpg';
             //$decodedImage->move(public_path('image'), $licenseFileName);
+            $license = 'image/' . $licenseFileName;
 
-            $license = 'images/' . $licenseFileName;
-            
 
         // Create user
         $user = User::create([
@@ -47,7 +45,7 @@ class UserController extends Controller
             'email' => $userData['email'],
             'mobile_number' => $userData['mobile_number'],
             'password' => bcrypt($userData['password']),
-           
+
             // Add other user fields as needed
         ]);
 
@@ -63,10 +61,8 @@ class UserController extends Controller
             'license_number'=>$merchantData['license_number'],
             // Add other merchant fields as needed
         ]);
-        file_put_contents(public_path( $passport ), $decodedImagePassport);
-        file_put_contents(public_path( $license  ), $decodedImagePassport);
-
-
+        file_put_contents(public_path( $passport ), $decodedPassportImage);
+        file_put_contents(public_path( $license  ), $decodedLicenseImage);
         return response()->json(['success' => true]);
     }
 
