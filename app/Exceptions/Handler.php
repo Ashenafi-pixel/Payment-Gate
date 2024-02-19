eb224f3c92737e7028c65b574133b81b4fc3a9d9
Ashenafi Alemu <ashenafialemu27@gmail.com>
2024-02-19 09:30:04 +0000

    Update Handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,13 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Session\TokenMismatchException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\TooManyRequestsHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -46,5 +53,47 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Throwable  $exception
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Throwable
+     */
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof NotFoundHttpException) {
+            return response()->view('errors.404', [], 404);
+        }
+
+        if ($exception instanceof AuthenticationException) {
+            return response()->view('errors.401', [], 401);
+        }
+
+        if ($exception instanceof AuthorizationException) {
+            return response()->view('errors.403', [], 403);
+        }
+
+        if ($exception instanceof TokenMismatchException) {
+            return response()->view('errors.419', [], 419);
+        }
+
+        if ($exception instanceof TooManyRequestsHttpException) {
+            return response()->view('errors.429', [], 429);
+        }
+
+        if ($exception instanceof ModelNotFoundException) {
+            return response()->view('errors.custom', ['message' => 'The requested resource was not found.'], 404);
+        }
+
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return response()->view('errors.custom', ['message' => 'The requested method is not allowed.'], 405);
+        }
+
+        return parent::render($request, $exception);
     }
 }
